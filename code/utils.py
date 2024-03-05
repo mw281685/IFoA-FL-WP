bb007a72e18a95aa3285d67b68e1cf8531d949f3
dylanil <dylan.liew@gmail.com>
2024-03-05 17:33:07 +0000

    Move graph notebook functions to utils.py

--- code/utils.py
+++ code/utils.py
@@ -11,6 +11,8 @@ import seaborn as sns
 import run_config
 import matplotlib.ticker as mtick
 from run_config import NUM_FEATURES
+import architecture
+from skorch import NeuralNetRegressor, NeuralNet, callbacks
 
 DATA_PATH = run_config.dataset_config["path"]
 SEED = run_config.dataset_config["seed"]
@@ -518,3 +520,275 @@ def uniform_partitions(agents:int = 10):
             val_array_dictionary["X_val_{0}".format(i)] = val_array_split[i]
             pd.DataFrame(val_array_split[i][:, 0:NUM_FEATURES], columns=X_column_names).to_csv(f'../data/X_val_{i}.csv' , index=False)
             pd.DataFrame(val_array_split[i][:, NUM_FEATURES]).to_csv(f'../data/y_vl_{i}.csv' , index=False)
+
+def load_model(agent=-1, num_features=NUM_FEATURES):
+    
+    all_results_df = pd.read_csv('../results/all_results.csv')
+    top_results_df = all_results_df.loc[all_results_df['rank_test_score']==1]
+    top_results_dict = top_results_df[['agent', 'param_module__num_units_1', 'param_module__num_units_2']].set_index('agent').to_dict('index')
+
+    num_units_1 = list(top_results_dict[agent].items())[0][1]
+    num_units_2 = list(top_results_dict[agent].items())[1][1]
+
+    loaded_agent_model = NeuralNetRegressor(architecture.MultipleRegression(num_features, num_units_1, num_units_2).double())
+    loaded_agent_model.initialize()  # This is important!
+    loaded_agent_model.load_params(f_params=f'../ag_'+str(agent)+'/agent_'+str(agent)+'_model.pkl', 
+                                       f_optimizer=f'../ag_'+str(agent)+'/agent_'+str(agent)+'_opt.pkl', 
+                                       f_history=f'../ag_'+str(agent)+'/agent_'+str(agent)+'_history.json')
+    
+    return loaded_agent_model
+
+def frequency_conversion(FACTOR, df, freq_dictionary):
+      for key in freq_dictionary:
+            df[freq_dictionary[key]]=df[key]/df['Exposure']
+
+      df.insert(1,FACTOR+'_binned_midpoint',[round((a.left + a.right)/2,0) for a in df[FACTOR+'_binned']])
+
+def undummify(df, prefix_sep="_"):
+    cols2collapse = {
+        item.split(prefix_sep)[0]: (prefix_sep in item) for item in df.columns
+    }
+    series_list = []
+    for col, needs_to_collapse in cols2collapse.items():
+        if needs_to_collapse:
+            undummified = (
+                df.filter(like=col)
+                .idxmax(axis=1)
+                .apply(lambda x: x.split(prefix_sep, maxsplit=1)[1])
+                .rename(col)
+            )
+            series_list.append(undummified)
+        else:
+            series_list.append(df[col])
+    undummified_df = pd.concat(series_list, axis=1)
+    return undummified_df
+
+def create_test_data(): 
+    (X_train, X_val, X_test, y_train, y_val, y_test, X_column_names, scaler) = upload_dataset()
+    
+    MY_DATA_PATH = '../data'
+    
+    X_test_sc = pd.read_csv(MY_DATA_PATH + '/X_test.csv')
+    y_te = pd.read_csv(MY_DATA_PATH + '/y_test.csv')
+    X_column_names = X_test_sc.columns.tolist()
+
+    test_complete_data=np.column_stack((X_test_sc, y_te))
+
+    X_column_names.append('ClaimNb')
+
+    #Convert dataset of test data, actuals, and prediction back into dataframe
+
+    df_test=pd.DataFrame(data=test_complete_data,    # values
+                    columns=X_column_names)  # 1st row as the column names
+    
+    # Un one-hot encode Region and VehBrand
+    df_test = undummify(df_test)
+    df_test['VehBrand_number'] = df_test['VehBrand'].str[1:].astype(int)
+    df_test['Region_number'] = df_test['Region'].str[1:].astype(int)
+
+
+    df_test[['Area', 'VehPower', 'VehAge','DrivAge','BonusMalus','Density']]=scaler.inverse_transform(df_test[['Area', 'VehPower', 'VehAge','DrivAge','BonusMalus','Density']] )
+    
+    #Bin factors
+    factor_list = ['Area', 'VehPower', 'VehAge', 'DrivAge', 'BonusMalus', 'VehGas', 'Density', 'VehBrand_number', 'Region_number']
+    
+    # If fewer levels than MAX_BINSIZE no effective binning happens via the min() function, not used currently
+    MAX_BINSIZE = 20
+
+    # Automatic binning not used, could use qcut as well 
+    for i in factor_list:
+        df_test[i+'_binned'] = pd.cut(df_test[i], bins=min(len(df_test[i].unique()), MAX_BINSIZE), duplicates='drop')
+        #df_test[i+'_binned'] = pd.cut(df_test[i], bins=np.linspace(0, max(df_test[i]), min(21, len(df_test[i].unique()))), duplicates='drop')
+    
+    # Custom binning used instead to define more sensible bins
+    df_test['Area'+'_binned'] = pd.cut(df_test['Area'], bins=np.linspace(0, 7, 8), duplicates='drop')
+    df_test['VehPower'+'_binned'] = pd.cut(df_test['VehPower'], bins=np.linspace(0, 15, 16), duplicates='drop')
+    df_test['VehAge'+'_binned'] = pd.cut(df_test['VehAge'], bins=np.linspace(0, 20, 21), duplicates='drop')
+    df_test['DrivAge'+'_binned'] = pd.cut(df_test['DrivAge'], bins=np.linspace(0, 100, 21), duplicates='drop')
+    df_test['BonusMalus'+'_binned'] = pd.qcut(df_test['BonusMalus'], q=15, duplicates='drop') # note usin qcut here
+    df_test['Density'+'_binned'] = pd.cut(df_test['Density'], bins=np.linspace(0, 11, 12), duplicates='drop')
+    df_test['VehBrand_number'+'_binned'] = pd.cut(df_test['VehBrand_number'], bins=np.linspace(0, 15, 16), duplicates='drop')
+    df_test['Region_number'+'_binned'] = pd.cut(df_test['Region_number'], bins=np.linspace(0, 100, 21), duplicates='drop')
+    df_test['VehGas'+'_binned'] = pd.cut(df_test['VehGas'], bins=np.linspace(0, 2, 3), duplicates='drop')
+    
+    return X_test, y_test, df_test
+
+def create_df_test_pred(df_test, X_test, NUM_AGENTS, global_model, fl_model, agent_model_dictionary):
+    
+    # Global Model Predictions
+    y_pred = global_model.predict(X_test.astype(np.float64))
+    df_test['ClaimNb_pred']=pd.Series(y_pred.flatten())
+
+    # FL Model Predictions
+    y_fl_pred = fl_model.predict(th.tensor(X_test).float())
+    df_test['ClaimNb_fl_pred']=pd.Series(y_fl_pred.flatten().detach().numpy())
+
+    # Local Model Predictions
+
+    agent_prediction_dictionary = {}
+
+    for agents in range(NUM_AGENTS):
+        agent_prediction_dictionary["y_agent_{0}_pred".format(agents)] = agent_model_dictionary['loaded_agent_'+str(agents)+'_model'].predict(X_test.astype(np.float64))
+        df_test['ClaimNb_agent_'+str(agents)+'_pred']=pd.Series(agent_prediction_dictionary['y_agent_'+str(agents)+'_pred'].flatten())
+
+    return df_test
+
+def create_df_sum(df_test_pred, factor, NUM_AGENTS):
+
+    sum_list = ['Exposure',  'ClaimNb', 'ClaimNb_pred', 'ClaimNb_fl_pred']
+    sum_dictionary = {'ClaimNb':'Actual freq', 'ClaimNb_pred':'Freq pred global model', 'ClaimNb_fl_pred':'Freq pred FL model'}
+
+    for agents in range(NUM_AGENTS):
+        sum_list.append('ClaimNb_agent_'+str(agents)+'_pred')
+        sum_dictionary['ClaimNb_agent_'+str(agents)+'_pred']='Freq pred agent '+str(agents)+' model'
+
+    df_sum=df_test_pred.groupby([factor+'_binned'])[sum_list].sum().reset_index()
+
+    frequency_conversion(factor, df_sum, sum_dictionary)
+
+    # Remove rows with 0 exposure
+    df_sum = df_sum.loc[df_sum['Exposure']!=0]
+    df_sum = df_sum.reset_index(drop=True)
+
+    return df_sum
+
+def one_way_graph_comparison(factor, df_test_pred, agents_to_graph_list, NUM_AGENTS):
+
+        #NUM_AGENTS = len(agents_to_graph_list)
+
+        df_sum = create_df_sum(df_test_pred, factor, NUM_AGENTS)
+        
+        fig, ax = plt.subplots(figsize=(12, 8))
+
+        plt.plot(df_sum['Actual freq'],  
+                label='Actual freq',
+                marker='s',
+                markersize=10,
+                )
+        
+        for agents in agents_to_graph_list:
+                plt.plot(df_sum['Freq pred agent '+str(agents)+' model'],  
+                label='Freq pred agent '+str(agents)+' model',
+                marker='o',
+                markersize=5,
+                #linestyle=(0, (1, 10)),
+                linestyle='dotted',
+                )
+
+
+        plt.plot(df_sum['Freq pred FL model'],  
+                label='Freq pred FL model',
+                marker='o',
+                markersize=10,
+                #linestyle='dotted',
+                )
+        
+        plt.plot(df_sum['Freq pred global model'],  
+                label='Freq pred global model',
+                marker='s',
+                markersize=10,
+                #linestyle='dotted',
+                )
+
+        plt.legend(bbox_to_anchor=(1.08, 1), loc='upper left', borderaxespad=0)
+
+        plt.xlabel(factor+' binned')
+        plt.xticks(rotation = 75)
+        plt.ylabel('Frequency')
+        plt.title('Actual vs. Expected by '+factor)
+        plt.grid()
+
+
+        # Get second axis
+        ax2 = ax.twinx()
+
+        plt.bar(df_sum[factor+'_binned'].astype(str), 
+                df_sum['Exposure'], 
+                label='Exposure', 
+                color='y',
+                alpha=0.35
+                )
+
+        plt.ylabel('Exposure', color='y')
+        plt.xticks(rotation = 90)
+
+        vals = ax2.get_yticks()
+        ax2.set_yticklabels(['{:,.0f}'.format(x) for x in vals])
+
+        plt.legend(bbox_to_anchor=(1.08, 0), loc='upper left', borderaxespad=0)
+
+        plt.show()
+
+def double_lift_rebase(df_test_pred, model1, model2):
+
+    # Rename 
+    df_test_pred['ClaimNb_global_pred'] = df_test_pred['ClaimNb_pred']
+    # Compare models
+    df_test_pred[model1+'_vs_'+model2] = df_test_pred['ClaimNb_'+model1+'_pred']/df_test_pred['ClaimNb_'+model2+'_pred']
+    # Bin comparison
+    df_test_pred[model1+'_vs_'+model2+'_binned'] = pd.qcut(df_test_pred[model1+'_vs_'+model2], 10)
+    # Sum up predictions
+    sum_list = ['Exposure',  
+                'ClaimNb', 
+                'ClaimNb_pred', 
+                'ClaimNb_'+model1+'_pred',
+                'ClaimNb_'+model2+'_pred',
+                ]
+    
+    df_sum=df_test_pred.groupby([model1+'_vs_'+model2+'_binned'])[sum_list].sum().reset_index()
+    # Rebase
+    df_sum['ClaimNb_rebased'] = df_sum['ClaimNb']/df_sum['ClaimNb']
+    df_sum['ClaimNb_'+model1+'_pred'+'_rebased'] = df_sum['ClaimNb_'+model1+'_pred']/df_sum['ClaimNb']
+    df_sum['ClaimNb_'+model2+'_pred'+'_rebased'] = df_sum['ClaimNb_'+model2+'_pred']/df_sum['ClaimNb']
+    # Graph
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    plt.plot(df_sum['ClaimNb_rebased'],  
+            label='Actual freq',
+            marker='s',
+            markersize=10,
+            )
+
+    plt.plot(df_sum['ClaimNb_'+model2+'_pred'+'_rebased'],  
+            label='Freq pred '+model2+' model',
+            marker='o',
+            markersize=10,
+            #linestyle=(0, (1, 10)),
+            #linestyle='dotted',
+            )
+
+    plt.plot(df_sum['ClaimNb_'+model1+'_pred'+'_rebased'],  
+            label='Freq pred '+model1+' model',
+            marker='o',
+            markersize=10,
+            #linestyle='dotted',
+            )
+
+    plt.legend(bbox_to_anchor=(1.08, 1), loc='upper left', borderaxespad=0)
+
+    plt.xlabel('Model 1 Prediction Over Model 2 Prediction')
+    plt.xticks(rotation = 75)
+    plt.ylabel('Actual Over Expected')
+    plt.title('Double Lift Chart Comparing Model Performance')
+    plt.grid()
+
+
+    # Get second axis
+    ax2 = ax.twinx()
+
+    plt.bar(df_sum[model1+'_vs_'+model2+'_binned'].astype(str), 
+            df_sum['Exposure'], 
+            label='Exposure', 
+            color='y',
+            alpha=0.35
+            )
+
+    plt.ylabel('Exposure', color='y')
+    plt.xticks(rotation = 90)
+
+    vals = ax2.get_yticks()
+    ax2.set_yticklabels(['{:,.0f}'.format(x) for x in vals])
+
+    plt.legend(bbox_to_anchor=(1.08, 0), loc='upper left', borderaxespad=0)
+
+    plt.show()
