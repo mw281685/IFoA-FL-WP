4c4a63e64818ea4ec13253548c11aefc7f35cef9
Malgorzata <mw281685@students.mimuw.edu.pl>
2024-03-14 23:39:44 +0000

    results for architecture 15 20  with 350 rnds 10 epochs each

--- code/run_config.py
+++ code/run_config.py
@@ -3,21 +3,21 @@ import pandas as pd
 
 # Constants and Configuration
 IF_TRAIN_VAL = 1  # Include validation dataset in training
-QUANTISATION = 1
-SMPC_NOISE = 1  
+QUANTISATION = 0
+SMPC_NOISE = 0  
 EPOCHS = 10
-BATCH_SIZE = 500
+BATCH_SIZE = 1000
 NUM_FEATURES = 39
 LEARNING_RATE = 0.001
 
 # Server Configuration
 server_config = {
     "num_clients": 10,
-    "num_rounds": 5
+    "num_rounds": 350
 }
 
 # Constructing a unique run name based on configuration
-run_name = f'UP_{server_config["num_clients"]}ag_{server_config["num_rounds"]}rnd_{EPOCHS}ep_{QUANTISATION}qt_{SMPC_NOISE}SMPCn'
+run_name = f'Final_{server_config["num_clients"]}ag_{server_config["num_rounds"]}rnd_{EPOCHS}ep_{QUANTISATION}qt_{SMPC_NOISE}SMPCn'
 
 # Dataset Configuration
 dataset_config = {
@@ -31,8 +31,8 @@ DATA_PATH = dataset_config["path"]
 SEED = dataset_config["seed"]
 
 # Extracting top results for NUM_UNITS_1 and NUM_UNITS_2
-NUM_UNITS_1 = 10 
-NUM_UNITS_2 = 15 
+NUM_UNITS_1 = 15 
+NUM_UNITS_2 = 20 
 
 if __name__ == "__main__":
     print(f"Run name: {run_name}")
